6b01bf0fe87bf2968ecb9520ca49ecc3e8f1661b
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-08 23:22:55 +0500

    feat: added logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LogoImg from "../../assets/logo.png";
+const Logo = () => {
+  return (
+    <div>
+      <img src={LogoImg} alt='Jadoo' />
+    </div>
+  );
+};
+
+export default Logo;
