db0a34f4b0f6380770daeb9dfb60e897690c064c
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-13 23:53:43 +0500

    feat: implemented section heading

---
+++ src/components/Elements/Logo/SectionHeader/SectionHeader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SectionHeader = ({ title, subtitle }) => {
+  return (
+    <div>
+      <span className='text-primaryLight font-bold text-xl mb-5 inline-block'>
+        {subtitle}
+      </span>
+      <h2 className='text-primaryDark text-5xl font-bold '>{title}</h2>
+    </div>
+  );
+};
+
+export default SectionHeader;
