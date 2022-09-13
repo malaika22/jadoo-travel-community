b197263f1d50250defa866e2e461b956a6b7ccf5
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-13 23:52:36 +0500

    chore: added content

---
+++ src/constants/content.js
@@ -0,0 +1,19 @@
+export const services = [
+  {
+    title: "Calculated Weather",
+    details: "Built Wicket longer admire do barton vanity itself do in it.",
+  },
+  {
+    title: "Best Flights",
+    details: "Engrossed listening. Park gate sell they west hard for the.",
+  },
+  {
+    title: "Local Events",
+    details:
+      "Barton vanity itself do in it. Preferd to men it engrossed listening. ",
+  },
+  {
+    title: "Customization",
+    details: "We deliver outsource daviation services for military customers",
+  },
+];
