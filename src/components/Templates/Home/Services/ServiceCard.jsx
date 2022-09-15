b7f7e26ae384ab976a445914c20c23f16bfd121b
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-15 23:50:36 +0500

    feat: implemented service card

--- src/components/Templates/Home/Services/ServiceCard.jsx
+++ src/components/Templates/Home/Services/ServiceCard.jsx
@@ -11,16 +11,35 @@ const serviceCardVariant = {
   },
 };
 
+const cardHover = {
+  hover: {
+    height: "100px",
+  },
+};
+
 const ServiceCard = ({ img, title, details }) => {
   return (
     <motion.div
-      className=' hover:shadow-lg transition-all duration-300 cursor-pointer min-h-[200px]'
-      variants={serviceCardVariant}
+      className='h-[340px] relative'
       whileHover='hover'
       initial='initial'
     >
-      <img />
-      <div>{title}</div>
+      <motion.div
+        className='px-5 py-5 hover:shadow-lg transition-shadow duration-300 cursor-pointer min-h-[200px] w-[90%] bg-white ml-auto'
+        variants={serviceCardVariant}
+        whileHover='hover'
+        initial='initial'
+      >
+        <div className=' min-h-[154px] flex items-center'>
+          <img src={img} className='mx-auto' />
+        </div>
+        <div className='text-[#1E1D4C] font-bold'> {title}</div>
+        <p className='text-primaryLight font-semibold my-4'>{details}</p>
+      </motion.div>
+      <motion.div
+        className='bg-[#DF6951] absolute bottom-0 w-[100px] -z-10 rounded-tl-[30px] rounded-br-[10px]'
+        variants={cardHover}
+      ></motion.div>
     </motion.div>
   );
 };
