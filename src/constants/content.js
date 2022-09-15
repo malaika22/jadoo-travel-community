96c54412246771f5410cccb0f5c8364718003fb0
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-15 23:48:06 +0500

    chore: added steps section content

--- src/constants/content.js
+++ src/constants/content.js
@@ -35,3 +35,21 @@ export const destinations = [
     duration: "28",
   },
 ];
+
+export const steps = [
+  {
+    step: "Choose Destination",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna, tortor tempus. ",
+  },
+  {
+    step: "Make Payment",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna, tortor tempus. ",
+  },
+  {
+    step: "Reach Airport on Selected Date",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna, tortor tempus. ",
+  },
+];
