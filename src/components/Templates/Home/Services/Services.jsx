a9d48527a5d01ce9626f0d380bcb93d13e67b773
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-15 23:50:24 +0500

    feat: implemented services section

--- src/components/Templates/Home/Services/Services.jsx
+++ src/components/Templates/Home/Services/Services.jsx
@@ -10,38 +10,44 @@ import LocalEvents from "../../../../assets/services/local-events.png";
 import Customization from "../../../../assets/services/customization.png";
 import { services } from "../../../../constants/content";
 import ServiceCard from "./ServiceCard";
+import ReactVisibilitySensor from "react-visibility-sensor";
 const Services = () => {
   const Images = [CalculatedWeather, Flights, LocalEvents, Customization];
   const [visible, setVisible] = useState(false);
+
   return (
-    <div className='py-20'>
-      <Container>
-        <motion.div
-          animate={visible ? "visible" : "invisible"}
-          variants={opacityVariants}
-          initial='invisible'
-          whileInView={() => setVisible(true)}
-          transition={{ duration: 0.5 }}
-          className='text-center'
-        >
-          <SectionHeader title='We Offer Best Services' subtitle='Category' />
-        </motion.div>
-        <motion.div
-          animate={visible ? "visible" : "invisible"}
-          variants={opacityVariants}
-          initial='invisible'
-          whileInView={() => setVisible(true)}
-          transition={{ duration: 0.5 }}
-          className='text-center '
-        >
-          <div className='grid grid-cols-4 gap-x-5 py-10 max-w-7xl mx-auto'>
-            {services.map((service, i) => (
-              <ServiceCard key={service.title} img={Images[i]} {...service} />
-            ))}
-          </div>
-        </motion.div>
-      </Container>
-    </div>
+    <ReactVisibilitySensor
+      partialVisibility
+      onChange={(isVisible) => isVisible && setVisible(isVisible)}
+      offset={{ bottom: 300 }}
+    >
+      <div className='my-20'>
+        <Container>
+          <motion.div
+            animate={visible ? "visible" : "invisible"}
+            variants={opacityVariants}
+            initial='invisible'
+            transition={{ duration: 0.5 }}
+            className='text-center'
+          >
+            <SectionHeader title='We Offer Best Services' subtitle='Category' />
+          </motion.div>
+          <motion.div
+            animate={visible ? "visible" : "invisible"}
+            variants={opacityVariants}
+            initial='invisible'
+            transition={{ duration: 0.5, delay: 0.5 }}
+            className='text-center '
+          >
+            <div className='grid grid-cols-4  pt-20 py-10 max-w-7xl mx-auto'>
+              {services.map((service, i) => (
+                <ServiceCard key={service.title} img={Images[i]} {...service} />
+              ))}
+            </div>
+          </motion.div>
+        </Container>
+      </div>
+    </ReactVisibilitySensor>
   );
 };
 
