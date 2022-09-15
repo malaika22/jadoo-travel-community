4d94338325662c547a774a6a64a2bc157b06d24f
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-15 23:49:39 +0500

    feat: added Destinations + Steps + Companies + Contact sections

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Container from "./components/Modules/Container/Container";
+import Companies from "./components/Templates/Home/Companies/Companies";
+import Contact from "./components/Templates/Home/Contact/Contact";
+import Destinations from "./components/Templates/Home/Destinations/Destinations";
 import HeroSection from "./components/Templates/Home/HomeHero/HeroSection";
 import Services from "./components/Templates/Home/Services/Services";
+import Steps from "./components/Templates/Home/Steps/Steps";
 
 function App() {
   return (
     <Container>
       <HeroSection />
       <Services />
+      <Destinations />
+      <Steps />
+      <Companies />
+      <Contact />
     </Container>
   );
 }
