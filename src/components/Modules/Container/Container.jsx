5e002b1e9ab090abf5a23d780352e3a5f087713d
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-17 02:10:09 +0500

    fix: fixed minor issue

--- src/components/Modules/Container/Container.jsx
+++ src/components/Modules/Container/Container.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ({ children }) => {
   return (
-    <div className='container mx-auto px-2 overflow-hidden sm:px-6 h-full'>
+    <div className='container overflow-hidden mx-auto px-2  sm:px-6 h-full'>
       {children}
     </div>
   );
