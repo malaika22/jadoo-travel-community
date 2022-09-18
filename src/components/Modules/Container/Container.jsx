ed688d090beed07bd0b2e6cb614c32bdaf056bc2
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-19 01:25:57 +0500

    fix: fixed section header

--- src/components/Modules/Container/Container.jsx
+++ src/components/Modules/Container/Container.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ({ children }) => {
   return (
-    <div className='container overflow-hidden mx-auto px-2  sm:px-6 h-full'>
+    <div className='max-w-screen-xl overflow-hidden mx-auto px-2  sm:px-6 h-full'>
       {children}
     </div>
   );
