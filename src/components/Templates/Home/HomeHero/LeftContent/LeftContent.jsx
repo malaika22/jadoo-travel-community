cee7a51c85cc4463cee6fe2597a352fc08d3465f
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-26 21:42:11 +0500

    fix: fixed hero section transitions

--- src/components/Templates/Home/HomeHero/LeftContent/LeftContent.jsx
+++ src/components/Templates/Home/HomeHero/LeftContent/LeftContent.jsx
@@ -1,39 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import HeadingDecor from "../../../../../assets/heroSection/heading-decor.png";
+import ReactVisibilitySensor from "react-visibility-sensor";
+
 const LeftContent = () => {
+  const [visible, setVisible] = useState(false);
+  const translateVariants = {
+    invisible: {
+      x: -500,
+      opacity: 0,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+    },
+  };
   return (
-    <motion.div
-      initial={{ x: -500, opacity: 0 }}
-      whileInView={{
-        transition: { ease: "easeOut", duration: 2 },
-        x: 0,
-        opacity: 1,
-      }}
-      viewport={{ once: true }}
-      className='text-center md:text-left'
+    <ReactVisibilitySensor
+      partialVisibility
+      offset={{ top: 400 }}
+      onChange={(isVisible) => isVisible && setVisible(isVisible)}
     >
-      <div className='text-[#DF6951] font-bold text-xl py-5'>
-        Best Destinations around the world
+      <div className='overflow-hidden'>
+        <motion.div
+          initial='invisible'
+          animate={visible ? "visible" : "invisible"}
+          variants={translateVariants}
+          transition={{ duration: 1.5 }}
+          className='text-center md:text-left'
+        >
+          <div className='text-[#DF6951] font-bold text-xl py-5'>
+            Best Destinations around the world
+          </div>
+          <div className='text-primary font-bold text-5xl  sm:text-6xl '>
+            Travel,{" "}
+            <div className='inline-block w-fit relative'>
+              <span>enjoy</span>
+              <img
+                src={HeadingDecor}
+                alt='Heading decor'
+                className='absolute w-full'
+              />
+            </div>{" "}
+            and live a new and full life
+          </div>
+          <p className='text-primaryLight text-lg font-semibold md:w-[500px] py-10'>
+            Built Wicket longer admire do barton vanity itself do in it.
+            Preferred to sportsmen it engrossed listening. Park gate sell they
+            west hard for the.
+          </p>
+        </motion.div>
       </div>
-      <div className='text-primary font-bold text-5xl  sm:text-6xl '>
-        Travel,{" "}
-        <div className='inline-block w-fit relative'>
-          <span>enjoy</span>
-          <img
-            src={HeadingDecor}
-            alt='Heading decor'
-            className='absolute w-full'
-          />
-        </div>{" "}
-        and live a new and full life
-      </div>
-      <p className='text-primaryLight text-lg font-semibold md:w-[500px] py-10'>
-        Built Wicket longer admire do barton vanity itself do in it. Preferred
-        to sportsmen it engrossed listening. Park gate sell they west hard for
-        the.
-      </p>
-    </motion.div>
+    </ReactVisibilitySensor>
   );
 };
 
--- src/components/Templates/Home/HomeHero/RightContent/RightContent.jsx
+++ src/components/Templates/Home/HomeHero/RightContent/RightContent.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import HeroDecor from "../../../../../assets/heroSection/hero-decor.png";
+import ReactVisibilitySensor from "react-visibility-sensor";
 
 const RightContent = () => {
+  const [visible, setVisible] = useState(false);
+
+  const translateVariants = {
+    invisible: {
+      x: 500,
+      opacity: 0,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+    },
+  };
   return (
-    <motion.div
-      initial={{ x: 500, opacity: 0 }}
-      whileInView={{
-        transition: { ease: "easeOut", duration: 2 },
-        x: 0,
-        opacity: 1,
-      }}
-      viewport={{ once: true }}
+    <ReactVisibilitySensor
+      partialVisibility
+      onChange={(isVisible) => isVisible && setVisible(isVisible)}
+      offset={{ top: 400 }}
     >
-      <img src={HeroDecor} className='w-[620px] ' />
-    </motion.div>
+      <div>
+        <motion.div
+          initial='invisible'
+          animate={visible ? "visible" : "invisible"}
+          variants={translateVariants}
+          transition={{ duration: 1.5 }}
+        >
+          <img src={HeroDecor} className='w-[620px] ' />
+        </motion.div>
+      </div>
+    </ReactVisibilitySensor>
   );
 };
 
